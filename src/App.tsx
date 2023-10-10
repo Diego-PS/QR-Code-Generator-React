dd0aacba7b4103377fa6722cc37552f497d13073
Diego-PS <diegopsilvavrb@gmail.com>
2023-10-10 20:13:20 -0300

    qr-code-generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import QRCode from 'react-qr-code'
+import styled from 'styled-components'
+
+const Input = styled.input`
+  font-size: large;
+  border-radius: 25px;
+  padding: 8px;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [text, setText] = useState('')
+  const handleChange: React.ChangeEventHandler<HTMLInputElement>  = (e) => {
+    setText(e.target.value)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h1>QR Code Generator</h1>
+      <QRCode value={text}></QRCode>
+      
+      <div className="input-here">
+        <p>Enter your text here</p>
+        <Input type="text" value={text} onChange={handleChange}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
